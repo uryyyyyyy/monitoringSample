e742bce64bb036fa678256714309ca3cd5786715
ko.shibata <ko.shibata@opt.ne.jp>
2016-09-21 18:42:59 +0900

    init

--- helloWorld/src/main/scala/com/github/uryyyyyyy/monitoring/hello/Main.scala
+++ helloWorld/src/main/scala/com/github/uryyyyyyy/monitoring/hello/Main.scala
@@ -1,9 +1,27 @@
 package com.github.uryyyyyyy.monitoring.hello
 
+import com.newrelic.api.agent.Trace
+import com.newrelic.api.agent.NewRelic
+
 object Main {
 
   def main (args: Array[String] ): Unit = {
+    NewRelic.setTransactionName(null, "/myTransaction")
 
+    Thread.sleep(1000)
+
+    for(i <- 1 to 1000){
+      val r = func1(1000)
+      println("hello" + r)
+    }
   }
 
+
+  @Trace
+  def func1(time: Int) = {
+    Thread.sleep(time)
+    "world"
+  }
 }
+
+
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,3 @@
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.4.3")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.14.3")
